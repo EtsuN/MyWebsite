c896cc21c35559b9b2d1e01a6ecb3a36b0d865a8
EtsuN <etnakaha@ucsd.edu>
2018-12-31 17:05:28 +0800

    collapse fix

---
+++ js/mywebsite.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+	initializePage();
+});
+
+function initializePage() {
+	$(".nav-link").click(function () {
+	    $(".collapse").collapse('toggle');
+	});
+
+/*
+	$("#locations").change(function () {
+	    var location = document.getElementById("locations");
+		var loc = "." + location.options[location.selectedIndex].value;
+		$("div.brewery-list").children().show();
+		if (loc != ".all") {
+			$("div.brewery-list").children().not(loc).hide();
+		}
+	});
+
+	$(".close-nav").click(function () {
+	    $(".sidenav").css("width", "0");
+	});
+
+	$(".brewery a").click(function () {
+	    localStorage.setItem('currentBrewery', $(this).next().text());
+	    localStorage.setItem('currentBreweryIndex', $(this).parent().attr('data-value'));
+	});	
+
+	$(".brewery h3, .brewery p").click(function () {
+	    window.location.href = "https://maps.google.com/?q=" + $(this).parent().find("h3").text();
+	});	
+*/
+}
