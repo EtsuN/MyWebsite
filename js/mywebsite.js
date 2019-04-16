75800bcb6203c276f1c5aa8816c4a0029df8d520
EtsuN <etnakaha@ucsd.edu>
2019-04-16 15:46:32 +0800

    improved content list

--- js/mywebsite.js
+++ js/mywebsite.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 	initializePage();
 	if ($(".carousel").length > 0)
 		setupCarousel();
+	if ($(".content-container").length > 0)
+		setupContents();
 
 	scrollEffect(); //this is needed to avoid blank page
 
@@ -55,7 +57,7 @@ function scrollEffect() {
 	var pageTop = $(document).scrollTop();
 	var pageBottom = pageTop + $(window).height();
 
-	var tags = $(".card, .container > h1, .container > .list-group");
+	var tags = $(".card, .container > h1, .container > .list-group, .flip-card");
 	for (var i = 0; i < tags.length; i++) {
 	    var tag = tags[i];
 	    var tagTop = $(tag).offset().top;
@@ -77,8 +79,17 @@ function scrollEffect() {
 
 var carouselData = {};
 
+carouselData['1'] = {
+	name: 'Escape Room',
+	name_ext: 'Escape Room (VR Game)',
+	description: '',
+	img: 'img/puzzle.png',
+	background: '',
+	span: '<span class="badge badge-secondary">Unity</span> <span class="badge badge-secondary">C#</span>'
+};
 carouselData['6'] = {
 	name: 'DarkVader',
+	name_ext: 'DarkVader (PC Game)',
 	description: 'DarkVader is a 3D computer game developed by OpenGL, with single player and multiplayer mode.',
 	img: 'img/darkvader.png',
 	background: 'background-black',
@@ -86,6 +97,7 @@ carouselData['6'] = {
 };
 carouselData['7'] = {
 	name: 'Insincere Quora Question Predictor',
+	name_ext: 'Insincere Quora Question Predictor',
 	description: 'Insincere Quora Question Predictor is a predictor that determines whether a Quora question is sincere or not developed by Python.',
 	img: 'img/quora.png',
 	background: '',
@@ -93,6 +105,7 @@ carouselData['7'] = {
 };
 carouselData['5'] = {
 	name: 'Beermenter',
+	name_ext: 'Beermenter (Mobile Website)',
 	description: 'Beermenter is a mobile website that helps user to find a brewery with beers that match to their preference.',
 	img: 'img/beermenter.png',
 	background: 'background-black',
@@ -100,6 +113,7 @@ carouselData['5'] = {
 };
 carouselData['2'] = {
 	name: 'Maya Archaeology Project',
+	name_ext: 'Maya Archaeology Project',
 	description: 'This project aims to develop a dual-camera system to generate 3D models of the excavated tunnels around the Maya temples.',
 	img: 'img/maya.png',
 	background: 'background-black',
@@ -107,11 +121,21 @@ carouselData['2'] = {
 };
 carouselData['3'] = {
 	name: 'Unilink',
+	name_ext: 'Unilink (Android App)',
 	description: 'Unilink is an Android app that provides an online platform where university students can make rental deals with each other and find their future roommates.',
 	img: 'img/unilink.png',
 	background: 'background-black',
 	span: '<span class="badge badge-secondary">Java</span> <span class="badge badge-secondary">Android Studio</span>'
 };
+carouselData['4'] = {
+	name: 'Cafeteria Delivery App',
+	name_ext: 'Cafeteria Delivery App (Android App)',
+	description: '',
+	img: 'img/cafeteria.png',
+	background: '',
+	span: '<span class="badge badge-secondary">Java</span> <span class="badge badge-secondary">Android Studio</span> <span class="badge badge-secondary">MySQL</span>'
+};
+
 
 function setupCarousel() {
 	$(".carousel").carousel({
@@ -154,4 +178,31 @@ function getURLParameter(sParam) {
         }
     }
     return "6723";
+}
+
+function setupContents() {
+	var indexOrder = "1675234";
+
+	for (var i = 0; i < indexOrder.length; i++) { 
+		var data = carouselData[indexOrder[i]];
+		if (i % 3 == 0)
+			$('.content-container').append(
+				'<div class="responsive-row"></div>'
+			);	
+
+	    $('.content-container .responsive-row').last().append(
+	      '<div class="responsive-col">' +
+	        '<div class="flip-card"><div class="flip-card-inner shadow">' +
+		  	  '<div class="flip-card-front center-parent" style="background-image: url(' + data["img"] + '); background-size: cover;">' +
+		        '<h3 class="center-child">' + data["name"] + '</h3> ' +
+		      '</div>' +
+		      '<div class="flip-card-back center-parent"><div class="center-child">' +
+		        '<h4>' + data["name_ext"] + '</h4>' +
+		        '<h4>' + data["span"] + '</h4>' +
+		        '<img src="img/goto.png" style="max-height: 50px; object-fit: contain;">' +
+		      '</div></div>' +
+	        '</div></div>' +
+	      '</div>'
+	    ); 
+	}	
 }
